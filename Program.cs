d623541975bd0e4b8ffbe7b0f8473e7e6846f5c2
Krosdir <pokemoon2698@gmail.com>
2019-02-24 18:26:53 +0700

    External sorting

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,62 +21,103 @@ namespace sort_it
         static T[] Merge<T>(T[] mass1, T[] mass2, string sortType) where T : IComparable<T>
         {
             int a = 0, b = 0;
+            
             T[] merged = new T[mass1.Length + mass2.Length];
             for (int i = 0; i < mass1.Length + mass2.Length; i++)
             {
-                if (sortType == "-a")
+                try
                 {
                     if (b.CompareTo(mass2.Length) < 0 && a.CompareTo(mass1.Length) < 0)
-                        if (mass1[a].CompareTo(mass2[b]) > 0)
+                        if ((int.Parse(mass1[a].ToString()) < int.Parse(mass2[b].ToString()) && (sortType == "-d")) || (!(int.Parse(mass1[a].ToString()) < int.Parse(mass2[b].ToString())) && !(sortType == "-d")))
+                        {
                             merged[i] = mass2[b++];
+                            Console.WriteLine("changed from b massiv, line {0}",i);
+                        }
                         else
+                        {
                             merged[i] = mass1[a++];
+                            Console.WriteLine("changed from a massiv, line {0}", i);
+                        }
                     else
                         if (b < mass2.Length)
+                        {
                             merged[i] = mass2[b++];
+                            Console.WriteLine("changed from b massiv, line {0}", i);
+                        }
                         else
+                        {
                             merged[i] = mass1[a++];
+                            Console.WriteLine("changed from a massiv, line {0}", i);
+                        }
                 }
-                else if (sortType == "-d")
+                catch
                 {
-                    if (b.CompareTo(mass2.Length) < 0 && a.CompareTo(mass1.Length) < 0)
-                        if (mass1[a].CompareTo(mass2[b]) > 0)
-                            merged[i] = mass1[a++];
-                        else
+                    if (b.CompareTo(mass2.Length) < 0 && a.CompareTo(mass1.Length) < 0) 
+                        if (((mass1[a].CompareTo(mass2[b]) < 0) && (sortType == "-d")) || (!(mass1[a].CompareTo(mass2[b]) < 0) && !(sortType == "-d")))
+                        {
                             merged[i] = mass2[b++];
+                            Console.WriteLine("changed from b massiv, line {0}", i);
+                        }
+                        else
+                        {
+                            merged[i] = mass1[a++];
+                            Console.WriteLine("changed from a massiv, line {0}", i);
+                        }
                     else
                         if (b < mass2.Length)
+                        {
                             merged[i] = mass2[b++];
+                            Console.WriteLine("changed from b massiv, line {0}", i);
+                        }
                         else
+                        {
                             merged[i] = mass1[a++];
+                            Console.WriteLine("changed from a massiv, line {0}", i);
+                        }
                 }
             }
             return merged;
         }
 
+        static void SplitFile(string pathFile, long fileLength, long partSize, string sortType)
+        {
+            var totalRead = 0;
+            using (var fs = new FileStream(pathFile, FileMode.Open, FileAccess.ReadWrite))
+            {
+                while (totalRead < fileLength)
+                {
+                    var part = new byte[fileLength - totalRead < partSize ? fileLength - totalRead : partSize];
+
+                    totalRead += fs.Read(part, 0, part.Length);
+                    using (var fsNew = new FileStream((Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\" + "temp.txt"), FileMode.Append))
+                    {
+                        fsNew.Write(part, 0, part.Length);
+                        
+                    }
+                    SortFile(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\" + "temp.txt", sortType);
+                    
+                }
+                using (var srIn = new StreamReader(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\" + "temp.txt"))
+                {
+                    string line;
+                    using (var swOut = new StreamWriter(fs))
+                    {
+                        while ((line = srIn.ReadLine()) != null)
+                        {
+                            swOut.WriteLine(line);
+                        }
+                    }
+                }
+                File.Delete(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\" + "temp.txt");
+            }
+        }
+
         static void SortFile(string pathFile, string sortType)
         {
-            List<int> intList = new List<int>();
-            int intValue = 0;
-            for (int q = 0; q < File.ReadAllLines(pathFile, Encoding.Default).ToList().Count; q++)
-            {
-                if (int.TryParse((File.ReadAllLines(pathFile, Encoding.Default).ToList()[q]), out intValue))
-                    intList.Add(intValue);
-            }
             if (sortType == "-a")
-            {
-                if (intList.Count == File.ReadAllLines(pathFile, Encoding.Default).ToList().Count)
-                    File.WriteAllLines(pathFile, MergeSort(intList.Select(x => int.Parse(x.ToString())).ToArray(), "-a").Select(x => x.ToString()).ToArray());
-                else
-                    File.WriteAllLines(pathFile, MergeSort(File.ReadAllLines(pathFile, Encoding.UTF8).ToArray(), "-a"));
-            }
+                File.WriteAllLines(pathFile, MergeSort(File.ReadAllLines(pathFile, Encoding.UTF8).ToArray(), "-a"));
             else if (sortType == "-d")
-            {
-                if (intList.Count == File.ReadAllLines(pathFile, Encoding.Default).ToList().Count)
-                    File.WriteAllLines(pathFile, MergeSort(intList.Select(x => int.Parse(x.ToString())).ToArray(), "-d").Select(x => x.ToString()).ToArray());
-                else
-                    File.WriteAllLines(pathFile, MergeSort(File.ReadAllLines(pathFile, Encoding.UTF8).ToArray(), "-d"));
-            }
+                File.WriteAllLines(pathFile, MergeSort(File.ReadAllLines(pathFile, Encoding.UTF8).ToArray(), "-d"));
         }
 
         static void Main(string[] args)
@@ -94,7 +136,7 @@ namespace sort_it
             try
             {
                 if (args.Length < 3)
-                    throw new Exception("Недостаточно аргументов (как минимум 3: тип данных, выходной файл, входной файл)");
+                    throw new Exception("Insufficient arguments (at least 3: data type, output file, input file)");
                 else
                 {
                     Console.WriteLine();
@@ -106,13 +148,14 @@ namespace sort_it
                             {
                                 isTyped = true;
                                 if (args[i] == "-s")
+                                {
                                     Console.WriteLine("TYPEOFDATA: string");
+                                }
                                 else
                                     Console.WriteLine("TYPEOFDATA: integer");
-                                //Метод для определения типа
                             }
                             else
-                                throw new Exception("Необходимо указать только один параметр с обозначением типа данных");
+                                throw new Exception("You must specify only one parameter designating the data type.");
                         }
                         else if (args[i] == "-a" || args[i] == "-d")
                         {
@@ -121,23 +164,22 @@ namespace sort_it
                                 isSortTyped = true;
 
                                 if (args[i] == "-a")
-                                    Console.WriteLine("TYPEOFSORT: По возрастанию");
+                                    Console.WriteLine("TYPEOFSORT: Ascending");
                                 else
                                 {
-                                    Console.WriteLine("TYPEOFSORT: По убыванию");
+                                    Console.WriteLine("TYPEOFSORT: Descending");
                                     sortType = "-d";
                                 }
-                                //Метод для определения типа сортировка
                             }
                             else
-                                throw new Exception("Необходимо указать только один параметр с обозначением типа сортировки или не указывать вовсе(по умолчанию сортировка по возрастанию)");
+                                throw new Exception("It is necessary to specify only one parameter with the designation of the sorting type or not to specify at all (by default, sorting is ascending)");
                         }
                         else if (args[i].Length > 2)
                         {
                             if (isOut == false)
                             {
                                 string[] argWords = args[i].Split('.');
-                                outFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/" + args[i].ToString();
+                                outFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\" + args[i].ToString();
                                 fsOut = new FileStream(outFile, FileMode.Append);
                                 swOut = new StreamWriter(fsOut);
                                 isOut = true;
@@ -146,10 +188,21 @@ namespace sort_it
                             }
                             else
                             {
-                                string inFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/" + args[i].ToString();
+                                string inFile = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\" + args[i].ToString();
                                 if (File.Exists(inFile))
                                 {
-                                    SortFile(inFile, sortType);
+                                    long fileLength = new FileInfo(inFile).Length / 1024 /1024;
+                                    
+                                    PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+                                    float available = ramCounter.NextValue();
+                                    ramCounter.Dispose();
+
+                                    Console.WriteLine("FILE_LENGTH: " + fileLength);
+                                    Console.WriteLine("AVAILABLE_RAM: " + available);
+                                    if (fileLength > available)
+                                        SplitFile(inFile, fileLength, (fileLength / (fileLength / (long)(available))), sortType);
+                                    else
+                                        SortFile(inFile, sortType);
                                     StreamReader srIn = new StreamReader(inFile);
                                     isIn = true;
                                     Console.WriteLine("INFILE: " + args[i]);
@@ -162,31 +215,31 @@ namespace sort_it
                                     srIn.Close();
                                 }
                                 else
-                                    throw new Exception("Отсутствует считывываемый файл(ы) с данным(и) именем(ми)");
+                                    throw new Exception("Missing readable file(s) with given name(s)");
                             }
                         }
                         else
                         {
-                            throw new Exception("Некорректный параметр(ы)");
+                            throw new Exception("Invalid parameter(s)");
                         }
                     }
                     if (isIn == false)
-                        throw new Exception("Отсутствует считываемый файл(ы)");
+                        throw new Exception("Missing file(s) read");
                     if (isSortTyped == false)
-                        Console.WriteLine("TYPEOFSORT: По убыванию");
+                        Console.WriteLine("TYPEOFSORT: Descending");
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Ошибка: {e.Message}");
+                Console.WriteLine("Error: {0}", e.Message);
             }
             finally
             {
                 swOut.Close();
                 fsOut.Close();
-                SortFile(outFile,sortType);
+                SortFile(outFile, sortType);
+                Console.WriteLine("Task competed");
             }
-
         }
     }
 }
